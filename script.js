99386ec953f81873773e2e75c12210502cb35348
wipchoptokyo <sendagaya2@icloud.com>
2025-07-21 17:34:00 +0900

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onload = () => {
-  const log = document.getElementById('log');
-  if (log) {
-    log.textContent = "✅ JSが読み込まれた";
-    console.log("✅ Consoleにも出力");
-  } else {
-    console.error("❌ #log が見つからない");
-  }
-};
